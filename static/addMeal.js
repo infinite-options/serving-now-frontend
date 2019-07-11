0af7f40d9842da6dcee2afa622d2cc4513c1674f
saurabh mane <manesaurabh987@gmail.com>
2019-07-10 23:28:55 -0700

    New design with fixed upload new image button

--- static/addMeal.js
+++ static/addMeal.js
@@ -38,3 +38,19 @@ function postMeal() {
     request.open("POST", "/kitchens/meals/create", true);
     request.send(formData);
 }
+
+function previewFile() {
+    var preview = document.querySelector('.img-bg'); //selects the query named img
+    var file = document.querySelector('input[type=file]').files[0]; //sames as here
+    var reader = new FileReader();
+
+    reader.onloadend = function() {
+        preview.src = reader.result;
+    }
+    if (file) {
+        reader.readAsDataURL(file); //reads the data as a URL
+    } else {
+        preview.src = "Upload Photo";
+    }
+}
+previewFile();
