d009b4e3adf164ee133c2250a1507e357dcc9743
saurabh mane <manesaurabh987@gmail.com>
2019-07-11 19:19:29 -0700

    added favorite button

--- static/deleteMeal.js
+++ static/deleteMeal.js
@@ -1,16 +1,12 @@
 function deleteMeal() {
     console.log('deleteMeal function called');
     var request = new XMLHttpRequest();
-    //var formData = new FormData();
 
     var delete_meal = document.getElementById("delete-meal-btn");
 
     var delete_meal_id = delete_meal.getAttribute("data-meal_id");
 
-    //formData.append('meal_id', delete_meal_id);
-    //console.log(formData);
-
-    request.open("POST", 'kitchen/meals/delete' + delete_meal_id, /* async = */ true);
+    request.open("POST", 'kitchen/meals/delete/' + delete_meal_id, /* async = */ true);
     request.send();
     console.log(request.response);
 }
