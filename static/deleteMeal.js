f48998c8cd11045b7b3ba115029f72c724dc7347
Purva <purva@Purvas-MacBook-Pro.local>
2019-07-18 11:58:34 -0700

    Updated delete function to remove images form S3 bucket and implemented DELETE http method to delete the meal

--- static/deleteMeal.js
+++ static/deleteMeal.js
@@ -3,7 +3,7 @@ function deleteMeal(id) {
 
     //async has to be false because the webpage will refresh before the
     //database can be updated                          Here
-    request.open("GET", '/api/v1/meals/' + id, /* async = */ false);
+    request.open("DELETE", '/api/v1/meals/' + id, /* async = */ false);
     request.send();
     window.location = "/kitchens/" + id;
 }
